5a31c3e327d72d4dc44d8143a924605fef2c9375
polong-lin <polong.lin+github@gmail.com>
2014-09-24 13:48:03 -0400

    Added descriptive comments

--- adencamera.R
+++ adencamera.R
@@ -1,5 +1,11 @@
+## Purpose of this script:
+## Retrieves df of Canon lenses and their prices from Aden Camera's website
+## Mines using XML package, and refines df using regular expressions
+
 #ADENCAMERA.COM
 require(stringr)
+require(XML)
+require(RCurl)
 
 #Canon lenses
 url <- "http://www.adencamera.com/default.asp?Category=7&Manufacturer=125&showall=yes"
@@ -22,15 +28,56 @@ productLink <- gsub(" ?Canon ?", "", productLink)
 productLink <- gsub(" Lens", "", productLink)
 productLink_gsub <- str_trim(gsub("\\*\\* .+\\*\\*", "", productLink))
 
-#Regex out Notes from Product
-sale_regmatch <- function(x) {
-  if(length(regmatches(x, regexpr("\\*\\* .+\\*\\*", x))) == 0) {
+#Regex function
+data.regmatch <- function(x, pattern) {
+  if(length(regmatches(x, regexpr(as.character(pattern), x))) == 0) {
     return("")
   }
-  regmatches(x, regexpr("\\*\\* .+\\*\\*", x))
+  regmatches(x, regexpr(as.character(pattern), x))
 }
-notes <- str_trim(gsub("\\*","",unlist(lapply(productLink, sale_regmatch))))
 
+#Regex out Notes from Product
+sale_pattern <- "\\*\\* .+\\*\\*"
+notes <- str_trim(gsub("\\*","",unlist(lapply(productLink, data.regmatch, sale_pattern))))
+
+#Regex EF, EF-S, TS-E from productLink
+type_pattern <- "EF |EF-S |TS-E|MP-E |EF-M"
+lens_type <- str_trim(unlist(lapply(d$Product, data.regmatch, type_pattern)))
+#d$Type <- lens_type
+
+#Regex focal length in mm
+focallength_pattern <- " ([0-9]*[-]+)?[0-9]+mm"
+focal_length <- unlist(lapply(d$Product, data.regmatch, focallength_pattern))
+#d["Focal Length"] <- focal_length
+
+#Regex aperture
+aperture_pattern <- " [fF](/)?([0-9.]{1,3})?(.)?([0-9.]{1,3})(L)?( )?"
+aperture <- gsub("f", "", unlist(lapply(d$Product, data.regmatch, aperture_pattern)))
+aperture <- gsub("/", "", aperture)
+aperture <- gsub("L", "", aperture)
+aperture <- str_trim(aperture)
+#d$Aperture <- aperture
+
+#Regex L-series
+Lseries_pattern <- "L "
+Lseries <- unlist(lapply(d$Product, data.regmatch, Lseries_pattern))
+#d$Luxury <- Lseries
+
+
+#Regex IS
+is_pattern <- " IS "
+is <- str_trim(unlist(lapply(d$Product, data.regmatch, is_pattern)))
+#d$IS <- is
+
+#Regex Mark
+mark_pattern <- " II "
+mark <- unlist(lapply(d$Product, data.regmatch, mark_pattern))
+#d$Mark <- mark
+
+#Regex Motor USM/STM
+motor_pattern <- "USM|STM"
+motor <- unlist(lapply(d$Product, data.regmatch, motor_pattern))
+#d$Motor <- motor
 
 #Regex prices out of HTML form
 patternPrice = "[0-9]+[,]?[0-9]+.[0-9][0-9]"
@@ -38,17 +85,39 @@ r <- regexec(patternPrice, ourPrice)
 regmatch <- regmatches(ourPrice, r)
 ourPriceList <- gsub(",","", unlist(regmatch))
 
-
-
+#Create df
 d<- data.frame("DateAccess" = rep(Sys.Date(), length(ourPriceList)), 
                "Brand" = rep("Canon", length(ourPriceList)),
-               "Product" = productLink_gsub, 
+               "Type" = lens_type,
+               "Focal Length" = focal_length,
+               "Aperture" = aperture,
+               "Luxury" = Lseries,
+               "IS" = is,
+               "Mark" = mark,
+               "Motor" = motor,
                "Price" = as.numeric(ourPriceList),
                "Notes" = notes)
-d <- d[with(d, order(Price, decreasing = TRUE)),]
-rownames(d) <- 1:nrow(d)
 
-head(d)
+d[complete.cases(d$Price,d$),]
+
+d.ordered <- d[with(d, order(Price, decreasing = TRUE)),]
+rownames(d.ordered) <- 1:nrow(d)
+#d
+head(d.ordered)
+
+d$Product
+
+
+
+
+
+
+
+
+
+
+
+
 # DateAccess Brand                               Product Price Notes
 # 1 2014-05-04 Canon EF 200-400mm f4L IS USM Extender 1.4x 11600  Sale
 # 2 2014-05-04 Canon                EF 800mm f/5.6L IS USM 10949  Sale
